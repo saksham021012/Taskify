903fd28d280fa157b07d4cec9ea7137da86351cb
saksham021012 <sakshamgoyal2k4@gmail.com>
2025-09-26 03:28:27 +0530

    fix: remove trailing slash from allowed origin for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 
 const allowedOrigins = [
     "http://localhost:5173",
-    "https://taskify-jade-one.vercel.app/"
+    "https://taskify-jade-one.vercel.app"
 ]
 
 app.use(cors({
